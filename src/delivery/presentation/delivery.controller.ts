6c1d36b04e12cb420c62799194a4c749ebba3174
Noemi Ancari <noemiancari.na@gmail.com>
2026-02-23 17:11:48 -0400

    instalacion de dependencias para comunicacion con apigateway

--- src/delivery/presentation/delivery.controller.ts
+++ src/delivery/presentation/delivery.controller.ts
@@ -1,118 +1,106 @@
-import { Controller, Post, Body, Param } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+
 import { AssingPackageToDealerCommand } from '../aplication/commands/assign-package-to-dealer.command';
 import { CreateDealerCommand } from '../aplication/commands/create-dealer.command';
 import { DeliverPackageCommand } from '../aplication/commands/deliver-package.command';
 import { CreatePackageCommand } from '../aplication/commands/create-package.command';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { CreateDealerDto } from './dto/create-dealer.dto';
-import { AssignPackageDto } from './dto/assign-package.dto';
-import { CreatePackageDto } from './dto/create-package.dto';
-import { AssignPackagesDto } from './dto/assign-packages.dto';
 import { CreateRouteWithPackagesCommand } from '../aplication/commands/create-route-with-packages.command';
 import { TransitPackageCommand } from '../aplication/commands/transit-package.command';
+import { CreateDealerDto } from './dto/create-dealer.dto';
 
-@ApiTags('Delivery')
-@Controller('delivery')
+@Controller()
 export class DeliveryController {
-    constructor(private readonly commandBus: CommandBus) { }
+    private readonly logger = new Logger(DeliveryController.name);
 
-    //CREAR REPARTIDOR
-  
-    @Post('create-dealer')
-    @ApiOperation({ summary: 'Crear un nuevo Repartidor', description: 'Crea un Repartidor con los datos proporcionados.' })
-    @ApiResponse({ status: 201, description: 'Dealer creado exitosamente.' })
-    @ApiResponse({ status: 400, description: 'Datos inválidos.' })
-    async createDealer(@Body() body: CreateDealerDto) {
+    constructor(private readonly commandBus: CommandBus) {}
+
+  // CREAR REPARTIDOR
+    @MessagePattern('create_dealer')
+    async createDealer(@Payload() body: CreateDealerDto) {
+        console.log('Mensaje recibido:', body);
         const command = new CreateDealerCommand(
-            body.identityCard,
-            body.firstName,
-            body.lastName,
-            body.cellPhone
+        body.identityCard,
+        body.firstName,
+        body.lastName,
+        body.cellPhone,
         );
 
         const dealer = await this.commandBus.execute(command);
         return { message: 'Dealer created successfully', dealer };
     }
 
-    //ASIGNAR PAQUETE REPARTIDOR 
-    @Post('assign-package')
-    @ApiOperation({ summary: 'Asignar paquete a repartidor', description: 'Asigna un paquete a un repartidor.' })
-    @ApiResponse({ status: 201, description: 'Paquete asignado correctamente.' })
-    @ApiResponse({ status: 400, description: 'Datos inválidos.' })
-    async assignPackage(
-        @Body() body: AssignPackageDto
-    ) {
+    // ASIGNAR PAQUETE
+    @MessagePattern('assign_package')
+    async assignPackage(@Payload() body: any) {
         const command = new AssingPackageToDealerCommand(
-            body.packageId,
-            body.dealerId,
-            new Date()
+        body.packageId,
+        body.dealerId,
+        new Date(),
         );
 
         await this.commandBus.execute(command);
+
         return { message: 'Package asignado correctamente' };
     }
 
-    //ENTREGAR PAQUETE
-    @Post(':id/deliver')
-    @ApiOperation({ summary: 'Marcar paquete como entregado', description: 'Marca un paquete como entregado usando su ID.' })
-    @ApiResponse({ status: 200, description: 'Paquete entregado exitosamente.' })
-    @ApiResponse({ status: 404, description: 'Paquete no encontrado.' })
-    async markAsDelivered(@Param('id') id: string) {
-
-        await this.commandBus.execute(
-            new DeliverPackageCommand(id),
-        );
+    // ENTREGAR PAQUETE
+    @MessagePattern('deliver_package')
+    async markAsDelivered(@Payload() id: string) {
+        await this.commandBus.execute(new DeliverPackageCommand(id));
 
         return {
-            status: true,
-            message: `Paquete ${id} marcado como entregado.`,
+        status: true,
+        message: `Paquete ${id} marcado como entregado.`,
         };
     }
 
-    //ENTREGAR PAQUETE
-    @Post(':id/transit')
-    @ApiOperation({ summary: 'Marcar paquete como en Camino', description: 'Marca un paquete como en camino usando su ID.' })
-    @ApiResponse({ status: 200, description: 'Paquete entregado exitosamente.' })
-    @ApiResponse({ status: 404, description: 'Paquete no encontrado.' })
-    async markAsTransit(@Param('id') id: string) {
-
-        await this.commandBus.execute(
-            new TransitPackageCommand(id),
-        );
+    // MARCAR EN CAMINO
+    @MessagePattern('transit_package')
+    async markAsTransit(@Payload() id: string) {
+        await this.commandBus.execute(new TransitPackageCommand(id));
 
         return {
-            status: true,
-            message: `Paquete ${id} marcado como en camino.`,
+        status: true,
+        message: `Paquete ${id} marcado como en camino.`,
         };
     }
 
-    //crear paquete 
-    @Post('create-package')
-    @ApiOperation({ summary: 'Crear paquete', description: 'Crea un paquete con los datos proporcionados.' })
-    @ApiResponse({ status: 201, description: 'Paquete creado correctamente.' })
-    @ApiResponse({ status: 400, description: 'Datos inválidos.' })
-    async createPackage(@Body() body: CreatePackageDto) {
+    // CREAR PAQUETE
+    @MessagePattern('create_package')
+    async createPackage(@Payload() body: any) {
         await this.commandBus.execute(
-            new CreatePackageCommand(
-                body.patientId,
-                new Date(body.deliveryDate),
-                body.addressStreet,
-                body.addressCity,
-                body.lat,
-                body.lng,
-                body.deliveryRouteId
-            )
+        new CreatePackageCommand(
+            body.patientId,
+            new Date(body.deliveryDate),
+            body.addressStreet,
+            body.addressCity,
+            body.lat,
+            body.lng,
+            body.deliveryRouteId,
+        ),
         );
-         return { message: 'Paquete creado correctamente' };
+
+        return { message: 'Paquete creado correctamente' };
     }
 
-    @Post('assign-packages-route')
-    @ApiOperation({ summary: 'Asignar paquetes a una ruta de Repartidor', description: 'Crea una nueva ruta y asigna los paquetes recibidos a dicha ruta.' })
-    @ApiResponse({ status: 201, description: 'Ruta creada exitosamente.' })
-    @ApiResponse({ status: 400, description: 'Error en los datos enviados.' })
-    async assignPackagesRoute(@Body() body: AssignPackagesDto) {
-      const command = new CreateRouteWithPackagesCommand(body.deliveryId,body.deliveryDate, body.packages);
-      return this.commandBus.execute(command);
+    // ASIGNAR RUTA
+    @MessagePattern('assign_packages_route')
+    async assignPackagesRoute(@Payload() body: any) {
+        const command = new CreateRouteWithPackagesCommand(
+        body.deliveryId,
+        body.deliveryDate,
+        body.packages,
+        );
+
+        return this.commandBus.execute(command);
+    }
+
+    @MessagePattern()
+    async handleUnknownMessage(@Payload() data: any) {
+        this.logger.warn(`Mensaje recibido sin manejador: ${JSON.stringify(data)}`);
+        return { error: 'No handler found for this message' };
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app =
+    await NestFactory.createMicroservice<MicroserviceOptions>(
+      AppModule,
+      {
+        transport: Transport.RMQ,
+        options: {
+          urls: [`amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASS}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`],
+          queue: 'delivery_queue',
+          queueOptions: { durable: false },
+        },
+      },
+    );
 
-  const config = new DocumentBuilder()
-  .setTitle('Delivery API')
-  .setDescription('API para gestión de delivery, paquetes y repartidores')
-  .setVersion('1.0')
-  .build();
-
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
-
-  await app.listen(process.env.PORT ?? 3006);
+  await app.listen();
 }
 bootstrap();
