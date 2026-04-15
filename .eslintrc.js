6e602fda6b90b7870ed60925ac19a372f2847f9a
Noemi Ancari <noemiancari.na@gmail.com>
2026-04-15 13:28:32 -0400

    subida de workflows

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,24 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['plugin:@typescript-eslint/recommended'],
-  rules: {
-    indent: ['error', 'tab'],
-    'no-mixed-spaces-and-tabs': 'error',
-    '@typescript-eslint/no-unsafe-assignment': 'warn',
-    '@typescript-eslint/no-unsafe-member-access': 'warn',
-    '@typescript-eslint/no-unsafe-argument': 'warn',
-    '@typescript-eslint/no-unsafe-call': 'warn',
-    '@typescript-eslint/no-floating-promises': 'warn',
-    '@typescript-eslint/require-await': 'warn'
-  }
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	extends: ['plugin:@typescript-eslint/recommended'],
+	overrides: [
+		{
+			files: ['**/*.spec.ts', '**/*.test.ts'],
+			rules: {
+				'@typescript-eslint/unbound-method': 'off',
+			},
+		},
+	],
+	rules: {
+		indent: ['error', 'tab'],
+		'no-mixed-spaces-and-tabs': 'error',
+		'@typescript-eslint/no-unsafe-assignment': 'warn',
+		'@typescript-eslint/no-unsafe-member-access': 'warn',
+		'@typescript-eslint/no-unsafe-argument': 'warn',
+		'@typescript-eslint/no-unsafe-call': 'warn',
+		'@typescript-eslint/no-unsafe-return': 'warn',
+		'@typescript-eslint/no-floating-promises': 'warn',
+		'@typescript-eslint/require-await': 'warn',
+	},
 };
